f6ddc276b311c6e1bdd5b22da35fb8cd588f2d68
Cullen Middleton <cullen.middleton@yoco.com>
2022-03-28 15:10:21 +0200

    Extra subtractNumbers tests

--- src/exercise-1.test.js
+++ src/exercise-1.test.js
@@ -38,6 +38,17 @@ describe("subtractNumbers", () => {
   it("should subtract numbers (0, -1000) correctly", () => {
     expect(subtractNumbers(0, -1000)).toBe(1000);
   });
+  it("should subtract numbers (5.5, 5.555) correctly", () => {
+    expect(subtractNumbers(5.5, 5.555)).toBeCloseTo(-0.055);
+  });
+  it("should subtract numbers (1/3, -1/3) correctly", () => {
+    expect(subtractNumbers(1 / 3, -1 / 3)).toBeCloseTo(2 / 3);
+  });
+  it("should subtract numbers (Number.MAX_VALUE, Number.MIN_VALUE) correctly", () => {
+    expect(subtractNumbers(Number.MAX_VALUE, Number.MIN_VALUE)).toBeCloseTo(
+      1.7976931348623157e308
+    );
+  });
 });
 
 describe("multiplyNumbers", () => {
